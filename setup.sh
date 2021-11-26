1c0b1b76efe097b3a56c0898de0556ff60277e9d
Lloyd Oliver <lloyd.oliver@suchdevops.co.uk>
2021-11-26 20:03:43 +0000

    updating setup script some more

--- setup.sh
+++ setup.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
+sopsver="3.7.1"
+gpgkeyid="0x10A16862F507DEB8"
 
-printf "\n Installing Dependencies \nThis may take a while, please wait ...\n"
-sudo apt-get install -qq -y software-properties-common git
+clear
+
+if [ ! -f /etc/udev/rules.d/70-u2f.rules ];then
+  printf "\nInstalling YubiKey udev rules...\n"
+  sudo curl -s https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules -o /etc/udev/rules.d/70-u2f.rules
+  printf "\n The system must be rebooted. Setup is NOT complete!\n\n \
+Please re-run this script after reboot to complete setup!\n\n"
+  sleep 5
+  sudo reboot
+fi
+
+printf "\n\nInstalling Dependencies\n  This may take a while, please wait ...\n"
+
+# Setup some repos and install some required packages
+sudo apt-get install -qq -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible &>/dev/null
 sudo apt-add-repository -y ppa:yubico/stable &>/dev/null
 sudo apt-get update -qq -y
-sudo apt-get install -qq -y ansible curl yubikey-manager yubikey-personalization cryptsetup hopenpgp-tools secure-delete gnupg2 gnupg-agent scdaemon pcscd dirmngr swig libssl-dev libpcsclite-dev wget
+sudo apt-get install -qq -y ansible gnupg2 gnupg-agent gnupg-curl scdaemon pcscd wget
 
-wget -q https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb
+# Download and install Mozilla SOPS (needed for ansible run)
+wget -q https://github.com/mozilla/sops/releases/download/"${sopsver}"/sops_"${sopsver}"_amd64.deb
 sudo dpkg -i sops_*_amd64.deb &>/dev/null
+
+# Setup GPG config
+mkdir ~/.gnupg &>/dev/null
+wget -q -P ~/.gnupg/ https://raw.githubusercontent.com/drduh/config/master/gpg.conf
+chmod 600 ~/.gnupg/gpg.conf
+
+# Get the GPG public key.
+gpg --recv "${gpgkeyid}" &>/dev/null || printf "\nFailed to retrieve GPG key!\n" ; exit 1
 
 printf "\n Done \n\n Running Ansible Playbook ...\n\n"
 sleep 1
 
+# Run Ansible
 ansible-playbook -i inventory.yml --ask-become-pass main.yml
