ce63b9194cea33bff547a1e47069bbdd7ab0d021
Lloyd Oliver <lloyd.oliver@suchdevops.co.uk>
2021-11-26 19:29:44 +0000

    make more stuff install silently

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-printf "\n Installing Dependencies, please wait ...\n"
+printf "\n Installing Dependencies \nThis may take a while, please wait ...\n"
 sudo apt-get install -qq -y software-properties-common git
 sudo apt-add-repository -y ppa:ansible/ansible &>/dev/null
 sudo apt-add-repository -y ppa:yubico/stable &>/dev/null
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y ansible curl yubikey-manager yubikey-personalization cryptsetup hopenpgp-tools secure-delete gnupg2 gnupg-agent scdaemon pcscd dirmngr swig libssl-dev libpcsclite-dev wget
 
-wget https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb
-sudo dpkg -i sops_*_amd64.deb
+wget -q https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb
+sudo dpkg -i sops_*_amd64.deb &>/dev/null
 
 printf "\n Done \n\n Running Ansible Playbook ...\n\n"
 sleep 1
